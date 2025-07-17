18c09c9b5fa1ae3c09a6af3931232a8458970f63
Степан Рудаков <19stepan94@mail.ru>
2025-07-17 22:18:28 +0500

    first commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+var nameUrl string
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+
+	nameUrl = r.URL.Path
+
+	fmt.Fprintln(w, nameUrl)
+
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, nameUrl)
+
+}
+
+func main() {
+	http.HandleFunc("/get", getHandler)
+	http.HandleFunc("/post", postHandler)
+
+	http.ListenAndServe("localhost:8080", nil)
+
+}
