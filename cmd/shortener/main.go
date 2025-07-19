d2c5338cc6a00b3d0510ecc109c6ba4dfb1aed79
Степан Рудаков <19stepan94@mail.ru>
2025-07-19 22:55:32 +0500

    создал функцию для генерации id, создал мапу в которую сохраняется ключ id со значение полученного url

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 )
+
+var urlMap = make(map[int64]string)
+
+func generateId() int64 {
+	idRand := rand.Int63()
+
+	fmt.Println("рандомный ID:", idRand)
+	return idRand
+
+}
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
@@ -18,6 +29,10 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		longUrl := string(body)
 		// выводим полученный URL
 		fmt.Fprintln(w, "Получили URL:", longUrl)
+		id := generateId()
+
+		urlMap[id] = longUrl
+		fmt.Fprintf(w, "http://localhost:8080/get/%d", id)
 	} else {
 		http.Error(w, "ошибка 400: не метод POST", http.StatusBadRequest)
 	}
@@ -29,9 +44,9 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	generateId()
 	http.HandleFunc("/", postHandler)
-	http.HandleFunc("/get", getHandler)
+	http.HandleFunc("/get/", getHandler)
 
 	http.ListenAndServe("localhost:8080", nil)
-
 }
