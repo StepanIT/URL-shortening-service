fc678220b434642e17d0bb04da9189ef9bd81fcf
Степан Рудаков <19stepan94@mail.ru>
2025-07-19 13:43:50 +0500

    написал функцию получения URL

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,28 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
-var nameUrl string
-
 func postHandler(w http.ResponseWriter, r *http.Request) {
-
-	nameUrl = r.URL.Path
-
-	fmt.Fprintln(w, nameUrl)
+	if r.Method == http.MethodPost {
+		// Читаем тело запроса
+		body, err := io.ReadAll(r.Body)
+		if err != nil || len(body) == 0 {
+			http.Error(w, "ошибка 400", http.StatusBadRequest)
+			return
+		}
+		// преобразуем URL из байтов в строку
+		longUrl := string(body)
+		// выводим полученный URL
+		fmt.Fprintln(w, "Получили URL:", longUrl)
+	} else {
+		http.Error(w, "ошибка 400: не метод POST", http.StatusBadRequest)
+	}
 
 }
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintln(w, nameUrl)
-
 }
 
 func main() {
+	http.HandleFunc("/", postHandler)
 	http.HandleFunc("/get", getHandler)
-	http.HandleFunc("/post", postHandler)
 
 	http.ListenAndServe("localhost:8080", nil)
 
