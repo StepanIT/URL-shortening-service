35cd08217523e0a03d71583766af1b1683e27ff2
Степан Рудаков <19stepan94@mail.ru>
2025-07-20 14:22:58 +0500

    создал функцию getHandler которая принимает сокращенный URL и возвращает оригинальный URL

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,32 +7,46 @@ import (
 	"net/http"
 )
 
-var urlMap = make(map[int64]string)
+// Мапа для хранения ID(сокращенный URL) и полный URL
+var urlMap = make(map[string]string)
 
-func generateId() int64 {
-	idRand := rand.Int63()
-
-	fmt.Println("рандомный ID:", idRand)
-	return idRand
-
+// функция для генерации ID
+func generateId() string {
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, 7)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
 }
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		// Читаем тело запроса
+
+		// читаем тело запроса
 		body, err := io.ReadAll(r.Body)
 		if err != nil || len(body) == 0 {
 			http.Error(w, "ошибка 400", http.StatusBadRequest)
 			return
 		}
+
 		// преобразуем URL из байтов в строку
 		longUrl := string(body)
+
 		// выводим полученный URL
 		fmt.Fprintln(w, "Получили URL:", longUrl)
+
+		// получаем ID
 		id := generateId()
 
+		// присваеваем полученный URL к полученному ID
 		urlMap[id] = longUrl
-		fmt.Fprintf(w, "http://localhost:8080/get/%d", id)
+
+		// выводим ответ с кодом 201 и сокращенный URL
+		w.WriteHeader(http.StatusCreated)
+		fmt.Fprintf(w, "http://localhost:8080/get/%s\n", id)
+
+		fmt.Fprintln(w, "мапа", urlMap)
 	} else {
 		http.Error(w, "ошибка 400: не метод POST", http.StatusBadRequest)
 	}
@@ -40,13 +54,37 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
 
+		// получаем ID из пути запроса, всё что идет после /get/
+		id := r.URL.Path[len("/get/"):]
+		if id == "" {
+			http.Error(w, "ошибка 400: пустой URL", http.StatusBadRequest)
+			return
+		}
+
+		// ищем оригинальный URL в мапе по полученному ID
+		originUrl, ok := urlMap[id]
+		if !ok {
+			http.Error(w, "ошибка 404: URL не найден", http.StatusNotFound)
+			return
+		}
+
+		// перенаправляет пользователя на оригинальный URL
+		http.Redirect(w, r, originUrl, http.StatusTemporaryRedirect)
+	} else {
+		http.Error(w, "ошибка 400: не метод GET", http.StatusBadRequest)
+	}
 }
 
-func main() {
-	generateId()
+// функция для создания сервера и обработчиков
+func handler() {
 	http.HandleFunc("/", postHandler)
 	http.HandleFunc("/get/", getHandler)
 
 	http.ListenAndServe("localhost:8080", nil)
+}
+
+func main() {
+	handler()
 }
