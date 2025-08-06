87edbf6748cf0cef3ce047194b50f0157343de6f
Степан Рудаков <19stepan94@mail.ru>
2025-08-06 22:37:46 +0500

    added comments

--- cmd/shortener/handlers/handlers_test.go
+++ cmd/shortener/handlers/handlers_test.go
@@ -40,15 +40,19 @@ func TestPostHandler_Success(t *testing.T) {
 
 // request body is empty
 func TestPostHandler_EmptyBody(t *testing.T) {
+	// create inMemory storage
 	repo := storage.NewInMemoryStorage()
 	h := &Handler{Repo: repo}
 
+	// create test request
 	w := httptest.NewRecorder()
 	c := getTestGinContext(w)
 	c.Request = httptest.NewRequest(http.MethodPost, "/", nil)
 
+	// call handler
 	h.PostHandler(c)
 
+	// checks
 	if w.Code != http.StatusBadRequest {
 		t.Errorf("ожидался статус 400 Bad Request, получили %d", w.Code)
 	}
@@ -56,6 +60,7 @@ func TestPostHandler_EmptyBody(t *testing.T) {
 
 // successful GET request
 func TestGetHandler_Success(t *testing.T) {
+	// create inMemory storage
 	repo := storage.NewInMemoryStorage()
 	id := "abc123"
 	url := "https://youtube.com"
@@ -64,13 +69,16 @@ func TestGetHandler_Success(t *testing.T) {
 	repo.Save(id, url)
 	h := &Handler{Repo: repo}
 
+	// create test request
 	w := httptest.NewRecorder()
 	c := getTestGinContext(w)
 	c.Request = httptest.NewRequest(http.MethodGet, "/get/"+id, nil)
 	c.Params = []gin.Param{{Key: "id", Value: id}}
 
+	// call handler
 	h.GetHandler(c)
 
+	// checks
 	if w.Code != http.StatusTemporaryRedirect {
 		t.Errorf("ожидался статус 307 Temporary Redirect, получили %d", w.Code)
 	}
@@ -84,15 +92,19 @@ func TestGetHandler_Success(t *testing.T) {
 
 // if the ID does not exist in the database
 func TestGetHandler_NotFound(t *testing.T) {
+	// create inMemory storage
 	repo := storage.NewInMemoryStorage()
 	h := &Handler{Repo: repo}
 
+	// create test request
 	w := httptest.NewRecorder()
 	c := getTestGinContext(w)
 	c.Request = httptest.NewRequest(http.MethodGet, "/get/неизвестныйID", nil)
 
+	// call handler
 	h.GetHandler(c)
 
+	// checks
 	if w.Code != http.StatusNotFound {
 		t.Errorf("ожидался статус 404 Not Found, получили %d", w.Code)
 	}
