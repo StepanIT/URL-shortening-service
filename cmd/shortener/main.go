5312eb1e6e3527e761e5202f2990ee12b6cb44e0
Степан Рудаков <19stepan94@mail.ru>
2025-08-15 22:27:34 +0500

    commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/StepanIT/URL-shortening-service/cmd/shortener/server"
+import (
+	"flag"
+
+	"github.com/StepanIT/URL-shortening-service/cmd/shortener/server"
+)
 
 func main() {
-	server.Handler()
+	addr := flag.String("a", "", "server address")
+	base := flag.String("b", "", "base URL")
+	file := flag.String("f", "", "file storage path")
+	flag.Parse()
+
+	server.Handler(*addr, *base, *file)
 }
