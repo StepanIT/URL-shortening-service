b0f25aff54e08758c8c9e1579c39cf074bf11768
Степан Рудаков <19stepan94@mail.ru>
2025-08-15 13:44:13 +0500

    commit

--- cmd/shortener/server/server.go
+++ cmd/shortener/server/server.go
@@ -1,14 +1,22 @@
 package server
 
 import (
+	"log"
+
 	config "github.com/StepanIT/URL-shortening-service"
 	"github.com/StepanIT/URL-shortening-service/cmd/shortener/handlers"
 	"github.com/StepanIT/URL-shortening-service/cmd/shortener/storage"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 // функция для создания сервера и обработчиков
 func Handler() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
 	cfg := config.NewConfig()
 
 	// создаём экхемпляр хранилища
